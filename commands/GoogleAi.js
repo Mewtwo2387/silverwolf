15c21622cf2df345136bf962ee6bc83ec289ec35
enzejoseph <nii.neko.nii.neko@gmail.com>
2024-08-12 13:24:13 +0800

    Summary (required)
    Description

--- commands/GoogleAi.js
+++ commands/GoogleAi.js
@@ -7,10 +7,10 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_TOKEN);
 
 class AskGeminiCommand extends Command {
     constructor(client) {
-        super(client, "ask-gemini", "wow this is so cool, should i add an ai art command ?", [
+        super(client, "ask-silverwolf-ai", "wow this is so cool, should i add an ai art command ?", [
             {
                 name: "prompt",
-                description: "The prompt to send to Gemini",
+                description: "The prompt",
                 type: 3,  // STRING type
                 required: true
             }
@@ -32,9 +32,10 @@ class AskGeminiCommand extends Command {
 
             // Create an embed to format the response
             const embed = new EmbedBuilder()
-                .setTitle('Gemini AI Response')
+                .setTitle('Silverwolf Ai says:')
                 .setDescription(text)
-                .setColor(0x0099ff);
+                .setColor(0x0099ff)
+                .setFooter({ text: 'Powered by ChatTGP', iconURL: 'https://media.discordapp.net/attachments/969953667597893675/1272422507533828106/Qzrb7Us.png?ex=66baeb4e&is=66b999ce&hm=cf4e7ed0da32e823e5ceb90cd94b1abf3e54cc19f447e38a0aef572af68cd04b&=&format=webp&quality=lossless&width=899&height=899' });
 
             // Edit the message with the actual response
             await interaction.editReply({ content: null, embeds: [embed] });
