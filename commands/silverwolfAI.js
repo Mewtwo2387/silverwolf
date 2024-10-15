a6166c897ea331c62c759e8309c5484982b775d3
mewtwo2387 <mewtwo2387@gmail.com>
2024-10-16 00:39:47 +0100

    c.ai integration... might or might not work?

---
+++ commands/silverwolfAI.js
@@ -0,0 +1,33 @@
+const { Command } = require("./classes/command.js");
+const { EmbedBuilder } = require("discord.js");
+
+class SilverwolfAI extends Command {
+    constructor(client){
+        super(client, "real-silverwolf-ai", "talk to silverwolf", [{
+            name: "message",
+            description: "the message to send to silverwolf",
+            type: 3,
+            required: true
+        }]);
+    }
+
+    async run(interaction){
+        if (!this.client.chat){
+            return await interaction.reply("Silverwolf AI is not loaded.")
+        }
+
+        const message = interaction.options.getString("message")
+
+        const response = await this.client.chat.sendAndAwaitResponse(message, true)
+
+        await interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle("Silverwolf says...")
+                .setDescription(response.text)
+            ]
+        });
+    }
+}
+
+module.exports = SilverwolfAI;
