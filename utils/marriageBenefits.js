e640aa9ede34c26b768243e7c9b38048192cff8a
XeIris <nii.neko.nii.neko@gmail.com>
2024-10-19 23:59:34 +0800

    divorce settlements

--- utils/marriageBenefits.js
+++ utils/marriageBenefits.js
@@ -5,7 +5,7 @@ async function marriageBenefits(client, uid, amt) {
     console.log('Checking user marriage status... for user ID:', uid);
     const userMarriageStatus = await client.db.checkMarriageStatus(uid);
 
-    // If the user is married, increase the amount by 20% (rounding up)
+    // If the user is married, increase the amount by 10% (rounding up)
     if (userMarriageStatus.isMarried) {
         console.log('User is married. Increasing the amount by 10%...');
         return Math.ceil(amt * 1.1);
