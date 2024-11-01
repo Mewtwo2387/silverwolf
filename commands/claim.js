62b50e1b647442ab540ea4eedc48b36c8c6834dd
XeIris <nii.neko.nii.neko@gmail.com>
2024-11-01 21:35:45 +0800

    Christmas themed claim

--- commands/claim.js
+++ commands/claim.js
@@ -30,43 +30,47 @@ class Claim extends Command {
         const multiplier = getMultiplierAmount(multiplier_amount_level);
         const { gold, silver, bronze } = getMultiplierChance(multiplier_rarity_level);
 //original
-        if (rand < gold) {
-            const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.gold);
-            return {
-                amount: amount,
-                title: `Congratulations! You've claimed a golden dinonuggie!! ${format(multiplier.gold, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
-                imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272801662121283614/AMuYswc.png?ex=66bc4c6b&is=66bafaeb&hm=1d284683c81389bf481ca100eb631a3b4d85ff51c86e22e7032f5cab30e73763&=&format=webp&quality=lossless&width=806&height=1169",
-                colour: '#FFD700',
-                footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`
-            };
-        } else if (rand < gold + silver) {
-            const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.silver);
-            return {
-                amount: amount,
-                title: `Congratulations! You've claimed a silver dinonuggie!! ${format(multiplier.silver, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
-                imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272804142871609445/r0LVjIF.png?ex=66bc4ebb&is=66bafd3b&hm=75fcdacc2e0e138e0ad0640d7328607fa8a692c626398bf19d8ce4631b4a63ef&=&format=webp&quality=lossless&width=433&height=629",
-                colour: '#C0C0C0',
-                footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`
-            };
-        } else if (rand < gold + silver + bronze) {
-            const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.bronze);
-            return {
-                amount: amount,
-                title: `Congratulations! You've claimed a bronze dinonuggie!! ${format(multiplier.bronze, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
-                imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272919852507463773/OXjd97e.png?ex=66bcba7e&is=66bb68fe&hm=34ef60370f5d26896aa8feca56846920228c78299144e9b5deb5d172522df56d&=&format=webp&quality=lossless&width=896&height=1169",
-                colour: '#CD7F32',
-                footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`
-            };
-        } else {
-            const amount = Math.ceil(await this.getBaseAmount(interaction, streak));
-            return {
-                amount: amount,
-                title: `${format(amount)} dinonuggies claimed!`,
-                imageUrl: "https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png",
-                colour: '#83F28F',
-                footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`
-            };
-        }
+        // if (rand < gold) {
+        //     const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.gold);
+        //     return {
+        //         amount: amount,
+        //         title: `Congratulations! You've claimed a golden dinonuggie!! ${format(multiplier.gold, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+        //         imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272801662121283614/AMuYswc.png?ex=66bc4c6b&is=66bafaeb&hm=1d284683c81389bf481ca100eb631a3b4d85ff51c86e22e7032f5cab30e73763&=&format=webp&quality=lossless&width=806&height=1169",
+        //         colour: '#FFD700',
+        //         footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+        //         thumbnail: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png'
+        //     };
+        // } else if (rand < gold + silver) {
+        //     const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.silver);
+        //     return {
+        //         amount: amount,
+        //         title: `Congratulations! You've claimed a silver dinonuggie!! ${format(multiplier.silver, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+        //         imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272804142871609445/r0LVjIF.png?ex=66bc4ebb&is=66bafd3b&hm=75fcdacc2e0e138e0ad0640d7328607fa8a692c626398bf19d8ce4631b4a63ef&=&format=webp&quality=lossless&width=433&height=629",
+        //         colour: '#C0C0C0',
+        //         footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+        //         thumbnail: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png'
+        //     };
+        // } else if (rand < gold + silver + bronze) {
+        //     const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.bronze);
+        //     return {
+        //         amount: amount,
+        //         title: `Congratulations! You've claimed a bronze dinonuggie!! ${format(multiplier.bronze, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+        //         imageUrl: "https://media.discordapp.net/attachments/1070612017058160731/1272919852507463773/OXjd97e.png?ex=66bcba7e&is=66bb68fe&hm=34ef60370f5d26896aa8feca56846920228c78299144e9b5deb5d172522df56d&=&format=webp&quality=lossless&width=896&height=1169",
+        //         colour: '#CD7F32',
+        //         footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+        //         thumbnail: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png'
+        //     };
+        // } else {
+        //     const amount = Math.ceil(await this.getBaseAmount(interaction, streak));
+        //     return {
+        //         amount: amount,
+        //         title: `${format(amount)} dinonuggies claimed!`,
+        //         imageUrl: "https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa",
+        //         colour: '#83F28F',
+        //         footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+        //         thumbnail: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png'
+        //     };
+        // }
 
 // halloween update
         // if (rand < gold) {
@@ -106,6 +110,49 @@ class Claim extends Command {
         //         footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`
         //     };
         // }
+
+// christmas update
+    if (rand < gold) {
+        const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.gold);
+        return {
+            amount: amount,
+            title: `Congratulations! You've claimed a golden dinonuggie!! ${format(multiplier.gold, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+            imageUrl: "https://drive.google.com/thumbnail?id=1U5oXdCW6LMjLgVxNWN-i4kDH_wow3yPp",
+            colour: '#FFD700',
+            footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+            thumbnail: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa'
+        };
+    } else if (rand < gold + silver) {
+        const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.silver);
+        return {
+            amount: amount,
+            title: `Congratulations! You've claimed a silver dinonuggie!! ${format(multiplier.silver, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+            imageUrl: "https://drive.google.com/thumbnail?id=1Q29dJ1ST1GMxmeM98jub4RKIpm-bZC1v",
+            colour: '#C0C0C0',
+            footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+            thumbnail: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa'
+        };
+    } else if (rand < gold + silver + bronze) {
+        const amount = Math.ceil(await this.getBaseAmount(interaction, streak) * multiplier.bronze);
+        return {
+            amount: amount,
+            title: `Congratulations! You've claimed a bronze dinonuggie!! ${format(multiplier.bronze, true)}x earned this claim for a total of ${format(amount)} dinonuggies!`,
+            imageUrl: "https://drive.google.com/thumbnail?id=1oLvRzxrs7b9Z97BVOacNbNCrodr68Fll",
+            colour: '#CD7F32',
+            footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+            thumbnail: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa'
+        };
+    } else {
+        const amount = Math.ceil(await this.getBaseAmount(interaction, streak));
+        return {
+            amount: amount,
+            title: `${format(amount)} dinonuggies claimed!`,
+            imageUrl: "https://drive.google.com/thumbnail?id=1_RPbZ680d8QFBv8oZMCwom6RrlgSUYBR",
+            colour: '#83F28F',
+            footer: `Gold: ${format(gold * 100, true)}% for ${format(multiplier.gold, true)}x | Silver: ${format(silver * 100, true)}% for ${format(multiplier.silver, true)}x | Bronze: ${format(bronze * 100, true)}% for ${format(multiplier.bronze, true)}x. Check upgrades with /upgrades`,
+            thumbnail: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa'
+        };
+    }       
     };
 
     async run(interaction) {
@@ -153,38 +200,38 @@ class Claim extends Command {
                     embeds: [
                         new Discord.EmbedBuilder()
                             .setTitle(selectedResponse.title)
-                            .setThumbnail('https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png')
+                            .setThumbnail('https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa')
                             .setDescription(`You can claim your next nuggie in ${cooldown - diff / HOUR_LENGTH} hours.`)
                             .setColor('#FF0000')
                             .setImage(selectedResponse.gifUrl)
-                            .setAuthor({ name: 'dinonuggie', iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
-                            .setFooter({ text: 'dinonuggie', iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
+                            .setAuthor({ name: 'dinonuggie', iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
+                            .setFooter({ text: 'dinonuggie', iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
                     ]
                 });
             } else if (diff > 2 * DAY_LENGTH) {
                 const amount = await this.getBaseAmount(interaction, 0);
                 await interaction.editReply({ embeds: [new Discord.EmbedBuilder()
-                    .setThumbnail('https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png')
+                    .setThumbnail('https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa')
                     .setTitle(`${format(amount)} dinonuggies claimed!`)
                     .setDescription(`You now have ${format(dinonuggies + amount)} dinonuggies. You broke your streak of ${streak} days.`)
                     .setColor('#83F28F')
-                    .setImage('https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png')
-                    .setAuthor({ name: 'dinonuggie', iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
-                    .setFooter({ text: 'dinonuggie', iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
+                    .setImage('https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa')
+                    .setAuthor({ name: 'dinonuggie', iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
+                    .setFooter({ text: 'dinonuggie', iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
                 ]});
                 await this.client.db.addUserAttr(interaction.user.id, 'dinonuggies', amount);
                 await this.client.db.setUserAttr(interaction.user.id, 'dinonuggies_last_claimed', now);
                 await this.client.db.setUserAttr(interaction.user.id, 'dinonuggies_claim_streak', 1);
             } else {
-                const { amount, title, imageUrl, colour, footer } = await this.getAmount(interaction, streak);
+                const { amount, title, imageUrl, colour, footer, thumbnail } = await this.getAmount(interaction, streak);
                 await interaction.editReply({ embeds: [new Discord.EmbedBuilder()
-                    .setThumbnail('https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png')
+                    .setThumbnail(thumbnail)
                     .setColor(colour)
-                    .setAuthor({ name: 'dinonuggie', iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
+                    .setAuthor({ name: 'dinonuggie', iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
                     .setTitle(title)
                     .setDescription(`You now have ${format(dinonuggies + amount)} dinonuggies. You are on a streak of ${streak + 1} days.`)
                     .setImage(imageUrl)
-                    .setFooter({ text: `dinonuggie | ${footer}`, iconURL: 'https://media.forgecdn.net/avatars/thumbnails/375/327/256/256/637550156004612442.png' })
+                    .setFooter({ text: `dinonuggie | ${footer}`, iconURL: 'https://drive.google.com/thumbnail?id=1oVDRweQoYLU6YfB01LWZpTFQiBS1fRRa' })
                 ]});
                 await this.client.db.addUserAttr(interaction.user.id, 'dinonuggies', amount);
                 await this.client.db.setUserAttr(interaction.user.id, 'dinonuggies_last_claimed', now);
