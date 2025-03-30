ef06f8080244e8a8d56da822d09ed865022a4cb4
hhw23 <hhw23@ic.ac.uk>
2025-03-30 16:35:07 +0100

    refactor[claim]: getAmount takes in uid instead of interaction so babySchedular can reuse

--- classes/babyScheduler.js
+++ classes/babyScheduler.js
@@ -1,42 +1,12 @@
 const cron = require('node-cron');
 const { log, logError } = require('../utils/log');
 require('dotenv').config(); 
-
+const Claim = require('../commands/claim');
 
 class BabyScheduler {
     constructor(client) {
         this.client = client;
     }
-
-    async getBaseAmount(uid) {
-      const nuggie_flat_multiplier_level = await this.client.db.getUserAttr(uid, 'nuggie_flat_multiplier_level');
-      const marriage_benefits = await marriageBenefits(this.client, uid);
-      
-      const nuggie_credits_multiplier_level = await this.client.db.getUserAttr(uid, 'nuggie_credits_multiplier_level');
-      const credits = await this.client.db.getUserAttr(uid, 'credits');
-      const log2_credits = credits > 1 ? Math.log2(credits) : 0;
-      
-      const nuggie_pokemon_multiplier_level = await this.client.db.getUserAttr(uid, 'nuggie_pokemon_multiplier_level');
-      const pokemon_count = await this.client.db.getUniquePokemonCount(uid);
-      
-      const nuggie_nuggie_multiplier_level = await this.client.db.getUserAttr(uid, 'nuggie_nuggie_multiplier_level');
-      const nuggies = await this.client.db.getUserAttr(uid, 'dinonuggies');
-      const log2_nuggies = nuggies > 1 ? Math.log2(nuggies) : 0;
-      
-      let baseAmount = 5
-      log("Base amount: " + baseAmount);
-      baseAmount *= getNuggieFlatMultiplier(nuggie_flat_multiplier_level)
-      log("Base amount after flat multiplier: " + baseAmount);
-      baseAmount *= (1 + log2_credits * getNuggieCreditsMultiplier(nuggie_credits_multiplier_level))
-      log("Base amount after credits multiplier: " + baseAmount);
-      baseAmount *= (1 + pokemon_count * getNuggiePokeMultiplier(nuggie_pokemon_multiplier_level))
-      log("Base amount after pokemon multiplier: " + baseAmount);
-      baseAmount *= (1 + log2_nuggies * getNuggieNuggieMultiplier(nuggie_nuggie_multiplier_level))
-      log("Base amount after nuggie multiplier: " + baseAmount);
-      baseAmount *= marriage_benefits
-      log("Base amount after marriage benefits: " + baseAmount);
-      return baseAmount;
-  }
     
     dailyAutomations(){
       cron.schedule('0 0 * * *', async () => {
@@ -47,7 +17,7 @@ class BabyScheduler {
               case "nuggie_claimer":
                 parents = [baby.mother_id, baby.father_id];
                 for (const parent of parents){
-                  baseAmount = await this.getBaseAmount(parent);
+                  baseAmount = await Claim.getBaseAmount(parent, 0);
                   await this.client.db.addUserAttr(parent, 'dinonuggies', baseAmount);
                   log(`${baby.name} (${baby.id}) claimed ${baseAmount} dinonuggies for ${parent}`);
                 }
