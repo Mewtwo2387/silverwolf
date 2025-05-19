fa716b0222cd883691b6efbd4562f43f3525e3bc
hhw23 <hhw23@ic.ac.uk>
2025-05-19 03:16:00 +0100

    refactor: command config table

---
+++ database/models/CommandConfigModel.js
@@ -0,0 +1,37 @@
+const { log } = require('../../utils/log');
+const commandConfigQueries = require('../queries/commandConfigQueries');
+
+class CommandConfigModel {
+  constructor(database) {
+    this.db = database;
+  }
+
+  async addOrUpdateCommandBlacklist(commandName, serverId, reason) {
+    const query = commandConfigQueries.ADD_OR_UPDATE_COMMAND_BLACKLIST;
+    await this.db.executeQuery(query, [commandName, serverId, reason]);
+    log(`Added or updated blacklist for command: ${commandName} in server: ${serverId}`);
+  }
+
+  async getBlacklistedCommands(serverId) {
+    const query = commandConfigQueries.GET_BLACKLISTED_COMMANDS;
+    return this.db.executeSelectAllQuery(query, [serverId]);
+  }
+
+  async deleteCommandBlacklist(commandName, serverId) {
+    const query = commandConfigQueries.DELETE_COMMAND_BLACKLIST;
+    const result = await this.db.executeQuery(query, [commandName, serverId]);
+    if (result.changes > 0) {
+      log(`Deleted blacklist entry for command: ${commandName} in server: ${serverId}`);
+      return `Successfully deleted the blacklist entry for command: ${commandName}`;
+    }
+    log(`No blacklist entry found for command: ${commandName} in server: ${serverId}`);
+    return `No blacklist entry found for command: ${commandName}`;
+  }
+
+  async isCommandBlacklisted(commandName, serverId) {
+    const blacklist = await this.getBlacklistedCommands(serverId);
+    return blacklist.some((command) => command.command_name === commandName);
+  }
+}
+
+module.exports = CommandConfigModel;
--- database/queries/serverRolesQueries.js
+++ database/queries/serverRolesQueries.js
@@ -2,7 +2,7 @@ const serverRolesQueries = {
   SET_SERVER_ROLE: 'INSERT OR REPLACE INTO ServerRoles (server_id, role_name, role_id) VALUES (?, ?, ?)',
   GET_SERVER_ROLE: 'SELECT role_id FROM ServerRoles WHERE server_id = ? AND role_name = ?',
   GET_ALL_SERVER_ROLES: 'SELECT role_name, role_id FROM ServerRoles WHERE server_id = ?',
-  REMOVE_SERVER_ROLE: 'DELETE FROM ServerRoles WHERE server_id = ? AND role_name = ?'
+  REMOVE_SERVER_ROLE: 'DELETE FROM ServerRoles WHERE server_id = ? AND role_name = ?',
 };
 
 module.exports = serverRolesQueries;
