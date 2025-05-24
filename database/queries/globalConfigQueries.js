8d927b1f2ba96901680539068676d12fde6d930d
hhw23 <hhw23@ic.ac.uk>
2025-05-24 11:13:22 +0100

    test: game uid and global config tests

--- database/queries/globalConfigQueries.js
+++ database/queries/globalConfigQueries.js
@@ -1,6 +1,6 @@
 const globalConfigQueries = {
   ADD_OR_UPDATE_GLOBAL_CONFIG: 'INSERT OR REPLACE INTO GlobalConfig (key, value) VALUES (?, ?)',
-  GET_GLOBAL_CONFIG: 'SELECT value FROM GlobalConfig WHERE key = ?',
+  GET_GLOBAL_CONFIG: 'SELECT key, value FROM GlobalConfig WHERE key = ?',
   GET_ALL_GLOBAL_CONFIG: 'SELECT key, value FROM GlobalConfig',
   DELETE_GLOBAL_CONFIG: 'DELETE FROM GlobalConfig WHERE key = ?',
 };
