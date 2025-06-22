187061dd7493f9d1dd07daf7dc762edec77b44eb
hhw23 <hhw23@ic.ac.uk>
2025-06-22 18:15:30 +0100

    feat[balance]: option to get other's balance

--- commands/balance.js
+++ commands/balance.js
@@ -5,20 +5,32 @@ const { log } = require('../utils/log');
 
 class Balance extends Command {
   constructor(client) {
-    super(client, 'balance', 'check your mystic credits and bitcoin', []);
+    super(client, 'balance', 'check your mystic credits and bitcoin', [
+      {
+        name: 'member',
+        description: 'member to check the balance of (default: you)',
+        type: 6,
+        required: false,
+      },
+    ]);
   }
 
   async run(interaction) {
-    const credits = await this.client.db.user.getUserAttr(interaction.user.id, 'credits');
-    const bitcoin = await this.client.db.user.getUserAttr(interaction.user.id, 'bitcoin');
-    const dinonuggies = await this.client.db.user.getUserAttr(interaction.user.id, 'dinonuggies');
-    const dinonuggiesStreak = await this.client.db.user.getUserAttr(interaction.user.id, 'dinonuggiesClaimStreak');
-    const heavenlyNuggies = await this.client.db.user.getUserAttr(interaction.user.id, 'heavenlyNuggies');
-    log(`${interaction.user.username} have ${format(credits)} mystic credits, ${bitcoin} bitcoin, ${format(dinonuggies)} dinonuggies, and ${format(heavenlyNuggies)} heavenly nuggies`);
+    const member = interaction.options.getMember('member') || interaction.user;
+    log(JSON.stringify(member));
+    const isSelf = member.id === interaction.user.id;
+    const credits = await this.client.db.user.getUserAttr(member.id, 'credits');
+    const bitcoin = await this.client.db.user.getUserAttr(member.id, 'bitcoin');
+    const dinonuggies = await this.client.db.user.getUserAttr(member.id, 'dinonuggies');
+    const dinonuggiesStreak = await this.client.db.user.getUserAttr(member.id, 'dinonuggiesClaimStreak');
+    const heavenlyNuggies = await this.client.db.user.getUserAttr(member.id, 'heavenlyNuggies');
+
+    const name = member.username ? member.username : member.displayName;
+    log(`${name} have ${format(credits)} mystic credits, ${bitcoin} bitcoin, ${format(dinonuggies)} dinonuggies, and ${format(heavenlyNuggies)} heavenly nuggies`);
     await interaction.editReply({
       embeds: [new Discord.EmbedBuilder()
         .setColor('#00AA00')
-        .setTitle(`You have ${format(credits)} mystic credits, ${bitcoin} bitcoin, ${format(dinonuggies)} dinonuggies, and ${format(heavenlyNuggies)} heavenly nuggies`)
+        .setTitle(`${isSelf ? 'You' : name} have ${format(credits)} mystic credits, ${bitcoin} bitcoin, ${format(dinonuggies)} dinonuggies, and ${format(heavenlyNuggies)} heavenly nuggies`)
         .setDescription(`Exact Credits: ${format(credits, false, 1000)}
 Exact Nuggies: ${format(dinonuggies, false, 1000)}
 Dinonuggies claim streak: ${dinonuggiesStreak}`)
