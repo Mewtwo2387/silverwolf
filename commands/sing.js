b3fd7809d262c250b6b00dffbdebd9ab1edc7dea
xei <137243135+XeIris@users.noreply.github.com>
2025-12-04 20:09:15 +0800

    Feat[Sing]: More songs
    you also now dont need to update choices manually for sing!

--- commands/sing.js
+++ commands/sing.js
@@ -2,6 +2,16 @@ const Discord = require('discord.js');
 const songs = require('../data/songs.json');
 const { Command } = require('./classes/command');
 
+const songChoices = Object.keys(songs).map((key) => {
+  const name = key
+    .replace(/([A-Z])/g, ' $1')
+    .trim();
+  return {
+    name,
+    value: key,
+  };
+});
+
 class Sing extends Command {
   constructor(client) {
     super(
@@ -13,46 +23,29 @@ class Sing extends Command {
         description: 'song to sing',
         type: 3,
         required: true,
-        choices: [
-          { name: 'If I Can Stop One Heart From Breaking', value: 'ifICanStopOneHeartFromBreaking' },
-          { name: 'Unauthorized Access', value: 'unauthorizedAccess' },
-          { name: 'Fly Me To The Moon', value: 'flyMeToTheMoon' },
-          { name: 'Women cheat', value: 'WomenCheat' },
-          { name: 'All I Want for Christmas Is You', value: 'AllIWantforChristmasIsYou' },
-        ],
+        choices: songChoices,
       }],
     );
   }
 
   async run(interaction) {
     if (this.client.singing) {
-      const embed = new Discord.EmbedBuilder()
-        .setColor('#AA0000')
-        .setTitle('No.')
-        .setDescription('Another song is in progress');
       await interaction.editReply({ embeds: [embed] });
       return;
     }
-
     this.client.singing = true;
-
     const song = interaction.options.getString('song');
     const lyrics = songs[song];
-
     await interaction.editReply(lyrics[0]);
-
     const delay = (ms) => new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
-
     await lyrics.slice(1).reduce(async (promise, lyric) => {
       await promise;
       await delay(1000);
       return interaction.channel.send(lyric);
     }, Promise.resolve());
-
     this.client.singing = false;
   }
 }
-
 module.exports = Sing;
